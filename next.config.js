dbbbb29bd6488b472b3d32e5eb601ccaa4615742
Ryo Ando <sasigume@gmail.com>
2021-01-19 14:26:14 +0900

    fixed version number not working on vercel

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ module.exports = {
         CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
         CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
         CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-        CONTENTFUL_PREVIEW_SECRET: process.env.CONTENTFUL_PREVIEW_SECRET,
-        REACT_APP_VERSION: process.env.REACT_APP_VERSION
+        CONTENTFUL_PREVIEW_SECRET: process.env.CONTENTFUL_PREVIEW_SECRET
     },
     images: {
         // This is required to show images with <Image /> component

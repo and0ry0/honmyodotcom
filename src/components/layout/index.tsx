c2b3ed3b199dec6e89d7606201d337f53697a1b3
Ryo Ando <sasigume@gmail.com>
2021-01-22 14:21:32 +0900

    change

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,43 @@
-import Footer from './footer'
-import Meta from './meta'
+
 import packageJson from '../../../package.json'
 
-export default function Layout({ isHome, preview, children }) {
+import React, { useState, useRef } from 'react';
+import { useOnClickOutside } from './menu';
+
+import { Burger, Menu } from './menu'
+import Footer from './footer'
+import Meta from './meta'
+
+import { CONST_SITE_NAME, CONST_MESSAGE } from '@options/constants'
+
+export default function Layout({ children, allUsers }) {
+
+  const [open, setOpen] = useState<boolean>();
+
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
 
   return (
     <>
       <Meta />
-      <div id="#" className="overflow-hidden max-w-screen overflow-y-scroll min-h-screen flex flex-col">
-        <main className="flex flex-col flex-grow items-center">
+      <div id="#" className="overflow-hidden w-screen h-screen">
+        <nav className="z-20 fixed top-0 left-0 flex">
+          <Burger open={open} setOpen={setOpen} />
+          <div className="p-3 text-red-400">
+            <div className="font-bold text-lg leading-loose">
+              <div className="text-xl">
+                {CONST_MESSAGE}
+                <br />
+                {CONST_SITE_NAME}
+              </div>
+            </div>
+          </div>
+        </nav>
+        {/* メニュー以外クリックで閉じる */}
+        <div ref={node} className="z-10 fixed top-20 left-0">
+          <Menu allUsers={allUsers} open={open} />
+        </div>
+        <main className="w-screen h-screen">
           {children}
         </main>
         <Footer />
@@ -17,3 +46,4 @@ export default function Layout({ isHome, preview, children }) {
     </>
   )
 }
+

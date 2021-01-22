54f9163665fe43ad20d1c00d98ac5d4df2147316
Ryo Ando <sasigume@gmail.com>
2021-01-23 00:24:08 +0900

    updated github

--- src/options/constants.ts
+++ src/options/constants.ts
@@ -6,5 +6,5 @@ export const CONST_SITE_NAME = "ほんみょ"
 export const CONST_SITE_URL = "https://" + domain
 export const CONST_MESSAGE = "架空の本棚を作るSNS"
 export const CONST_SITE_META = CONST_MESSAGE
-export const CONST_REPO_URL = "https://github.com/sasigume/honmyodotcom"
+export const CONST_REPO_URL = "https://github.com/and0ry0/honmyodotcom"
 export const CONST_BLOG_URL = "https://blog.honmyo.com"
